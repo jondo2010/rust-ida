8861382070b8856168d23b996291648d4a546ccf
John Hughes <john.hughes@aid-driving.eu>
2019-03-19 09:15:35 +0100

    * Initial dense_get_rf()

--- src/linear/dense.rs
+++ src/linear/dense.rs
@@ -18,6 +18,7 @@ where
     }
 }
 
+/*
 impl<M> LSolver2<M> for Dense<M>
 where
     M: ModelSpec,
@@ -47,6 +48,7 @@ where
 
     }
 }
+*/
 
 /// Performs the LU factorization of the M by N dense matrix A.
 ///
@@ -66,80 +68,86 @@ where
 /// For square matrices (M = N), L is unit lower triangular.
 ///
 /// returns 0 if successful. Otherwise it encountered a zero diagonal element during the factorization. In this case it returns the column index (numbered from one) at which it encountered the zero.
-fn dense_get_rf<M, S1>(matA: &mut ArrayBase<S1, Ix2>, p: &mut Vec<usize>)
+fn dense_get_rf<M, S1>(matA: &mut ArrayBase<S1, Ix2>, p: &mut Vec<usize>) -> usize
 where
     M: ModelSpec,
+    M::Scalar: num_traits::Float + num_traits::NumRef + num_traits::NumAssignRef + num_traits::Zero,
     S1: ndarray::DataMut<Elem = M::Scalar>,
 {
+    use num_traits::{Float, Zero};
     //sunindextype i, j, k, l;
     //realtype *col_j, *col_k;
     //realtype temp, mult, a_kj;
 
-    let (m, n) = matA.shape();
+    let (m, n) = {
+        let shape = matA.shape();
+        (shape[0], shape[1])
+    };
 
-    /* k-th elimination step number */
-    /*for (k=0; k < n; k++)*/
-    {
-        col_k = a[k];
+    // k-th elimination step number
+    for k in 0..n {
+        let mut col_k = matA.column_mut(k);
 
-        /* find l = pivot row number */
-        l = k;
-        //for (i=k+1; i < m; i++)
-        {
-            if (SUNRabs(col_k[i]) > SUNRabs(col_k[l])) {
+        // find l = pivot row number
+        let mut l = k;
+        for i in k + 1..m {
+            if col_k[i].abs() > col_k[l].abs() {
                 l = i;
             }
         }
         p[k] = l;
 
-        /* check for zero pivot element */
-        if (col_k[l] == ZERO) {
-            return (k + 1);
+        // check for zero pivot element
+        if col_k[l] == M::Scalar::zero() {
+            return k + 1;
         }
 
-        /* swap a(k,1:n) and a(l,1:n) if necessary */
+        // swap a(k,1:n) and a(l,1:n) if necessary
 
-        if (l != k) {
-            /*for (i=0; i<n; i++) */
+        if l != k {
+            for i in 0..n {
+                matA.swap([i, k], [i, l]);
+            }
+            /*
             {
                 temp = a[i][l];
                 a[i][l] = a[i][k];
                 a[i][k] = temp;
             }
+            */
         }
 
-        /* Scale the elements below the diagonal in
-         * column k by 1.0/a(k,k). After the above swap
-         * a(k,k) holds the pivot element. This scaling
-         * stores the pivot row multipliers a(i,k)/a(k,k)
-         * in a(i,k), i=k+1, ..., m-1.
-         */
-        mult = ONE / col_k[k];
-        //for(i=k+1; i < m; i++) {col_k[i] *= mult;}
+        // Scale the elements below the diagonal in
+        // column k by 1.0/a(k,k). After the above swap
+        // a(k,k) holds the pivot element. This scaling
+        // stores the pivot row multipliers a(i,k)/a(k,k)
+        // in a(i,k), i=k+1, ..., m-1.
 
-        /* row_i = row_i - [a(i,k)/a(k,k)] row_k, i=k+1, ..., m-1 */
-        /* row k is the pivot row after swapping with row l.      */
-        /* The computation is done one column at a time,          */
-        /* column j=k+1, ..., n-1.                                */
+        let mult = col_k[k].recip();
+        for i in k + 1..m {
+            col_k[i] *= mult;
+        }
 
-        /*for (j=k+1; j < n; j++)*/
-        {
-            col_j = a[j];
-            a_kj = col_j[k];
+        // row_i = row_i - [a(i,k)/a(k,k)] row_k, i=k+1, ..., m-1
+        // row k is the pivot row after swapping with row l.
+        // The computation is done one column at a time, column j=k+1, ..., n-1.
 
-            /* a(i,j) = a(i,j) - [a(i,k)/a(k,k)]*a(k,j)  */
-            /* a_kj = a(k,j), col_k[i] = - a(i,k)/a(k,k) */
+        for j in k + 1..n {
+            let mut col_j = matA.column_mut(j);
+            let a_kj = col_j[k];
 
-            if (a_kj != ZERO) {
-                //for (i=k+1; i < m; i++)
-                {
+            // a(i,j) = a(i,j) - [a(i,k)/a(k,k)]*a(k,j)
+            // a_kj = a(k,j), col_k[i] = - a(i,k)/a(k,k)
+
+            if a_kj != M::Scalar::zero() {
+                for i in k + 1..m {
                     col_j[i] -= a_kj * col_k[i];
                 }
             }
         }
     }
 
-    /* return 0 to indicate success */
+    // return 0 to indicate success
 
-    return (0);
+    return 0;
 }
